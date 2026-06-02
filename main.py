950f23d4c3d840422c6e12c3e5e08bdc09efbe50
as2305 <165639720+as2305@users.noreply.github.com>
2026-06-02 19:45:36 +0530

    Code fix

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pathlib import Path
 file_path = Path("kanban.json")
 
 #define JSON Structure
-kanban_structure = {
+kanban = {
   "To-do": [],
   "Doing": [],
   "Done": []
@@ -14,18 +14,36 @@ kanban_structure = {
 
 #check if the file exists, if not, create a new one and assign the core JSON stucture
 if not file_path.is_file():
-    file_path.write_text(json.dumps(kanban_structure, indent = 4))
+    file_path.write_text(json.dumps(kanban, indent = 4))
 
+kanban = json.loads(file_path.read_text())
 #initiate the main program and take user choice
 while True:
-  choice = input("\nWelcome to PyKanban! What would you like to do? \n 1) New Task \n 2) View Board \n >>>")
-  #Takes input and assigns it to kanban_structure
+  choice = input("\nWelcome to PyKanban! What would you like to do? \n 1) New Task \n 2) View Board \n 3) Move Task \n \n >>>")
+  #Takes input and assigns it to kanban
   if choice == "1":
     task_input = input("\nPlease enter the task: ")
-    kanban_structure["To-do"].append(task_input)
+    kanban["To-do"].append(task_input)
+    file_path.write_text(json.dumps(kanban, indent = 4))
   #Displays Kanban Board
   elif choice == "2":
     counter = 1
-    for i in kanban_structure["To-do"]:
+    print("\n------- KANBAN BOARD -------")
+    print("\n--- To-do ---")
+    if not kanban["To-do"]:
+      print("--Empty--")
+    for i in kanban["To-do"]:
+      print(f"{counter} --- {i}")
+      counter += 1
+    print("\n--- Doing ---")
+    if not kanban["Doing"]:
+      print("--Empty--")
+    for i in kanban["Doing"]:
+      print(f"{counter} --- {i}")
+      counter += 1
+    print("\n--- Done ---")
+    if not kanban["Done"]:
+      print("--Empty--")
+    for i in kanban["Done"]:
       print(f"{counter} --- {i}")
       counter += 1

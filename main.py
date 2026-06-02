7eafca86af2761eb20b498531831f9df50ff3656
as2305 <165639720+as2305@users.noreply.github.com>
2026-06-02 18:44:46 +0530

    Add user choices; add task and display kanban board

--- main.py
+++ main.py
@@ -16,5 +16,16 @@ kanban_structure = {
 if not file_path.is_file():
     file_path.write_text(json.dumps(kanban_structure, indent = 4))
 
-
-#Main Code Starts
+#initiate the main program and take user choice
+while True:
+  choice = input("\nWelcome to PyKanban! What would you like to do? \n 1) New Task \n 2) View Board \n >>>")
+  #Takes input and assigns it to kanban_structure
+  if choice == "1":
+    task_input = input("\nPlease enter the task: ")
+    kanban_structure["To-do"].append(task_input)
+  #Displays Kanban Board
+  elif choice == "2":
+    counter = 1
+    for i in kanban_structure["To-do"]:
+      print(f"{counter} --- {i}")
+      counter += 1
